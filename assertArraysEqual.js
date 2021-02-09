c674bba2a47f61b70594a852e9eddbe9d3ce0c83
Bradyn <bradyn.parisian@gmail.com>
2021-02-09 02:58:28 +0000

    refactored and separate file created for test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,5 @@
 
-const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arrayA, arrayB) => {
   if (eqArrays(arrayA, arrayB) === true) {
@@ -19,7 +9,7 @@ const assertArraysEqual = (arrayA, arrayB) => {
   }
 };
 
-assertArraysEqual([3, 2, 1], [3, 2, 1]);
-assertArraysEqual([3, 1, 1], [3, 2, 1]);
-
 module.exports = assertArraysEqual;
+
+
+
