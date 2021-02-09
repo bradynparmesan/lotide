f2931d02c3eb253206a4380d9a8025ff8197befa
Bradyn <bradyn.parisian@gmail.com>
2021-02-09 02:54:56 +0000

    eqArraysTest file created

---
+++ test/eqArraysTest.js
@@ -0,0 +1,17 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+
+//Test Cases
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+//Assertions
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should FAIL
