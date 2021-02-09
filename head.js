b344d5b7228d4d795968581ec8fd4b3d9924f8c0
Bradyn <bradyn.parisian@gmail.com>
2021-02-09 02:22:45 +0000

    refactored w/ exports

--- head.js
+++ head.js
@@ -1,10 +1,5 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const head = (array) => {
   firstElement = "";
