2278166687c371213b6cf59a785b9f74019fc0ad
Bradyn <bradyn.parisian@gmail.com>
2021-02-09 02:23:31 +0000

    test files created for head and assertEqual functions

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Happy", "Happy");
+assertEqual(3,4);
+assertEqual("Buzz", "Fizz");
+assertEqual(2,2);
