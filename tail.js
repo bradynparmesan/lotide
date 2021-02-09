1ae3bbd6f906d3b20404fc6e03124bf6aab05ce9
Bradyn <bradyn.parisian@gmail.com>
2021-02-09 02:42:09 +0000

    refactor with test file separated

--- tail.js
+++ tail.js
@@ -1,10 +1,5 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = require('assertEqual');
+const assertEqual = require('./assertEqual');
 
 const tail = (array) => {
   let tailArray = [];
@@ -13,8 +8,4 @@ const tail = (array) => {
   return tailArray;
 };
 
-// Test Case
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
