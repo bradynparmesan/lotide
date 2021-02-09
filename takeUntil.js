634efd6ff22ff4a4e370089f472b7caf92a95ce6
Bradyn <bradyn.parisian@gmail.com>
2021-02-09 02:02:28 +0000

    function to take slice of array up to defined point, then return new array

---
+++ takeUntil.js
@@ -0,0 +1,32 @@
+/* lotide library function - takeUntil.js - function to return a slice of array elements taken from the beginning of an array up to a specified point/character */
+
+const takeUntil = function(arr, callback) {
+  trueArr = [];
+  newArr = [];
+  for (const value of arr) {
+    trueArr.push(callback(value));
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (!trueArr[i]) {
+      newArr.push(arr[i]);
+    } else {
+      return newArr;
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+//expected output --> [1, 2, 5, 7, 2]
+
+console.log('---');
+
+const data2 =  ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+//expected output --> ['I\'ve', 'been', 'to', 'Hollywood']
+
+module.exports = takeUntil.js;
